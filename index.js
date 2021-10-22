ef24872fb06a0939e656fdb9eb41f3a6082a923c
Travis Crocker <70865828+Tmcrocker89@users.noreply.github.com>
2021-10-22 10:53:14 -0400

    updated to include readme syntax and readme questions

--- index.js
+++ index.js
@@ -1,32 +1,84 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is your user name?',
-      name: 'username',
-    },
-    {
-      type: 'input',
-      message: 'What languages do you know?',
-      name: 'languages',
-    },
-    {
-      type: 'input',
-      message: 'What is your preferred method of communication?',
-      name: 'method',
-    },
-  ])
-  .then((response) =>
+const questions = [
   {
-    fs.writeFile('log.txt', JSON.stringify(response), (err) =>
+      type: 'input',
+      message: 'What is the title of the project?',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: 'Give a description.',
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'How do you install the program?',
+      name: 'installation',
+    },
+    {
+      type: 'input',
+      message: 'How do you use the program?',
+      name: 'usage',
+    },
+    {
+      type: 'input',
+      message: 'which license?',
+      name: 'license',
+    },
+    {
+      type: 'input',
+      message: 'Who contribuited?',
+      name: 'contribution',
+    },
+    {
+      type: 'input',
+      message: 'Give test instructions.',
+      name: 'tests',
+    },
+    {
+      type: 'input',
+      message: 'Give your email, and github for questions to be directed to.',
+      name: 'questions',
+    },
+  
+];
+
+
+function writeToFile(fileName, {title, description, installation, usage, license, contribution, tests, questions}) 
+{
+  let readmeContent = 
+`
+# ${title}
+# Table of Contents
+${description}
+# Installation
+${installation}
+# Usage
+${usage}
+# License
+${license}
+# Contributing
+${contribution}
+# Tests
+${tests}
+# Questions
+${questions}
+`
+  fs.writeFile(fileName, readmeContent, (err) =>
        err ? console.error(err) : console.log('Success!')
     )
-  }
+}
+const init = () =>
+{
+  inquirer
+    .prompt(questions)
+    .then((response) =>
+    {
+      writeToFile('Readme.md', response)
+    }
 
 
-  );
-
-  //`What is your user name?: ${response.username}\nWhat languages do you know: ${response.languages}\nWhat is your preferred method of communication? ${response.method}\n`
+    );
+}
+init()
